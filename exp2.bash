8d7f795ce5a346436a745b779c0b054bfc1ccc0e
Lacey Jones <34206644+1acey@users.noreply.github.com>
2017-12-06 16:42:23 +0800

    Update exp2.bash

--- exp2.bash
+++ exp2.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #Program:
 #       Experiment2: User Login Monitor
 #History:
@@ -18,12 +18,12 @@ echo -e "Current user list is:\n"$userlist
 while [ true ]
 do
         userlist=`who | cut -d " " -f 1`
-        if echo "$1" | grep -q "$userlist"
+        if echo "$userlist" | grep -q "$1"
         then
                 echo  -e "\nuser[$1] is logon"
                 exit
         else
                 echo -e "\nwaiting user[$1] login...\c" #waiting for specified user to login
-                sleep 5s
+                sleep 5
         fi
 done
